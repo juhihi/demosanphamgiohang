5d8247f65415edd1fc204b7159e1ecaf844c4865
juhihi <ttai1632003@gmail.com>
2023-08-21 19:37:43 +0700

    demosanphamgiohang

---
+++ src/test/java/com/demogiohang/DemoLab8Java61ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demogiohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLab8Java61ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
